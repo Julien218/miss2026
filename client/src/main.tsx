d08a66bf2e8ee9de4a95100bb0b9d62817e85b12
Js-Innov.IA <info@jsinnovia.com>
2026-09-10 01:21:54 +0200

    feat(public): activate shared chrome across public routes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,7 @@ import { httpBatchLink, TRPCClientError } from "@trpc/client";
 import { createRoot } from "react-dom/client";
 import superjson from "superjson";
 import App from "./App";
+import { PublicSiteChrome } from "./components/PublicSiteChrome";
 import { getLoginUrl } from "./const";
 import "./index.css";
 import "./editorial-2027.css";
@@ -58,7 +59,9 @@ const trpcClient = trpc.createClient({
 createRoot(document.getElementById("root")!).render(
   <trpc.Provider client={trpcClient} queryClient={queryClient}>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <PublicSiteChrome>
+        <App />
+      </PublicSiteChrome>
     </QueryClientProvider>
   </trpc.Provider>
 );
