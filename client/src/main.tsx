e18f2cec6540ca4e65fe9536a73eac19dddf40d4
Js-Innov.IA <info@jsinnovia.com>
2026-09-11 00:39:49 +0200

    Charger styles journal 2027

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -21,6 +21,7 @@ import "./login-2027.css";
 import "./mobile-2027.css";
 import "./modern-ux-2027.css";
 import "./forms-2027.css";
+import "./articles-2027.css";
 
 const queryClient = new QueryClient();
 const redirectToLoginIfUnauthorized = (error: unknown) => {
