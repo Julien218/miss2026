9dc5272a1b37f8f08cf6d31d713a84dcf0d4f585
Js-Innov.IA <info@jsinnovia.com>
2026-09-26 20:35:32 +0200

    style: load mobile homepage overrides last

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -36,6 +36,7 @@ import "./final-polish-2027.css";
 import "./sponsor-transparent-2027.css";
 import "./showcase-2027.css";
 import "./sponsors-orbit-official-2027.css";
+import "./mobile-homepage-2027.css";
 
 const queryClient = new QueryClient();
 const redirectToLoginIfUnauthorized = (error: unknown) => {
